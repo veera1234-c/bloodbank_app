b6aef0e1142431ca7847a9078cf4e5804ebac463
@Veera1234-c <72505543+veera1234-c@users.noreply.github.com>
2023-01-18 18:50:25 +0530

    done with some changes

--- lib/screens/hello.dart
+++ lib/screens/hello.dart
@@ -7,7 +7,69 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(),
+        child: Column(
+          children: [
+            Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.teal.shade400,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      20,
+                    ),
+                  ),
+                  border: Border.all(width: 1, color: Colors.yellow),
+                ),
+                height: 80,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          child: Text("Task time details"),
+                        ),
+                        Container(
+                          child: Text("Task name"),
+                        )
+                      ],
+                    )
+                  ],
+                )),
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.teal.shade400,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  )),
+              height: 80,
+              width: double.infinity,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.teal.shade400,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  )),
+              height: 80,
+              width: double.infinity,
+            )
+          ],
+        ),
       ),
     );
   }
