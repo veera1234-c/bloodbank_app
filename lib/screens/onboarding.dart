9f2af8c5ee0ca1beb94d412ffd11ae0cf0761926
@Veera1234-c <72505543+veera1234-c@users.noreply.github.com>
2023-01-21 16:05:54 +0530

    done onboarding upto slider 3

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,3 +1,4 @@
+import 'package:bloodbank_app/constants/onboarding_data.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -9,18 +10,28 @@ class MyHomePage extends StatelessWidget {
         body: SafeArea(
       child: Container(
         color: Color(0xffFa4848),
-        child:
-            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          Image.asset("healthCheck.png"),
-          Container(
-            padding: EdgeInsets.only(left: 57, right: 56),
-            child: Text(
-              "To be a responsible donor, you must get a check-up.",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18),
-            ),
-          )
-        ]),
+        child: SingleChildScrollView(
+          child: Column(
+            children: onboardingData
+                .map(
+                  (onboardingSingleData) => Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(onboardingSingleData["image_url"]!),
+                      Container(
+                        padding: EdgeInsets.only(left: 57, right: 56),
+                        child: Text(
+                          onboardingSingleData["text"]!,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+                .toList(),
+          ),
+        ),
       ),
     ));
   }
